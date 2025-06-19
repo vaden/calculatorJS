6f37ff04e28890ae0d8d7c257f597c8b09e7d713
Quack Ranger <vaden130@gmail.com>
2025-06-19 05:45:05 +0200

    Adding JS + show the last result

--- js/app.js
+++ js/app.js
@@ -1,2 +1,23 @@
 const buttons = document.querySelectorAll(".touche");
 const affichage = document.querySelector("#affichage-calcul");
+const affichageMemoire = document.querySelector("#memoire-calcul");
+const memoire = [];
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const buttonValue = button.textContent;
+    if (buttonValue === "C") {
+      affichage.textContent = "";
+    } else if (buttonValue === "=") {
+      try {
+        affichage.textContent = eval(affichage.textContent);
+        memoire.push(affichage.textContent);
+        affichageMemoire.textContent = memoire[memoire.length - 1];
+      } catch {
+        affichage.textContent = "Erreur";
+      }
+    } else {
+      affichage.textContent += buttonValue;
+    }
+  });
+});
